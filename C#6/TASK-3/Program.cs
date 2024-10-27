c697ae64d2e31b574583b8b27e7027454055a94b
rgnrz231 <sharkragnar@gmail.com>
2024-10-27 20:48:22 +0000

    S6/TASK-3

---
+++ C#6/TASK-3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your word that you want to change: ");
+        string input = Console.ReadLine();
+
+        bool isPalindrome = IsPalindrome(input);
+
+        if (isPalindrome)
+        {
+            Console.WriteLine($"{input} is Palindrome.");
+        }
+        else
+        {
+            Console.WriteLine($"{input} isn't Palindrome.");
+        }
+    }
+    public static bool IsPalindrome(string str)
+    {
+        // Преобразуем строку в нижний регистр
+        string lowerResult = ChangeYouSymbToLower(str);
+
+        char[] charArray = lowerResult.ToCharArray();
+        Array.Reverse(charArray);
+        string reversedArray = new string(charArray);
+        return lowerResult == reversedArray;
+    }
+}
