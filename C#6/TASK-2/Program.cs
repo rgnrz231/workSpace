c12189a3afb1fcf9bd1a4ba029bd31c687c23c0c
rgnrz231 <sharkragnar@gmail.com>
2024-10-27 20:20:43 +0000

    S6/TASK-2

---
+++ C#6/TASK-2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your word that you want to change: ");
+        string str = Console.ReadLine(); 
+        string result = ChangeYouSymbToLower(str);
+        Console.WriteLine(result);
+    }
+
+    public static string ChangeYouSymbToLower(string arg)
+    {
+        string lower = arg.ToLower();
+        return lower;
+    }
+}
