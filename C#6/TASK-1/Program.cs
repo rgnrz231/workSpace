1fd7be4f0de02036d4e1894cabcdc7abd99d3c78
rgnrz231 <sharkragnar@gmail.com>
2024-10-27 20:02:58 +0000

    S6/TASK-1

---
+++ C#6/TASK-1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        char[,] array = {
+            {'a', 'b', 'c'},
+            {'d', 'e', 'f'}
+            };
+        string result = ConvertArrayToString(array);
+        Console.WriteLine(result);
+    }
+    public static string ConvertArrayToString(char[,] arr)
+    {
+        StringBuilder res = new StringBuilder(); // ""
+
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                res.Append(arr[i, j]);
+            }
+        }
+        return res.ToString();
+    }
+
+}
