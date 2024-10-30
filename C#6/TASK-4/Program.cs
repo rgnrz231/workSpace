39acaf922d5f2a443073ca90e7aa46cba1a1bf0b
rgnrz231 <sharkragnar@gmail.com>
2024-10-30 14:58:29 +0000

    S7/TASK-1

---
+++ C#6/TASK-4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your sentence that you want to reverse: ");
+        string input = Console.ReadLine();
+        string reverseResult = reverseString(input);
+
+        Console.WriteLine($"Your reversed sentece: {reverseResult}");
+    }
+    public static string reverseString(string str)
+    {
+        string[] spacedWords = str.Split(' '); // разбиение строки на слова
+
+        string result = string.Empty;
+        for (int i = spacedWords.Length - 1; i >= 0; i--)
+        {
+            result += spacedWords[i] + " ";
+        }
+        result = result.Trim(); // удаляет лишний пробел в конце
+
+        return result;
+    }
+}
+
+
---
+++ C#7/TASK-1/Program.cs
@@ -0,0 +1,27 @@
+﻿static void PrintNaturalNumbersRange(int firstNum, int secNum)
+{
+    if (firstNum <= secNum)
+    {
+        Console.Write($"{firstNum} ");
+        if (firstNum != secNum) // условие для предотвращения бесконечной рекурсии    
+        {
+            PrintNaturalNumbersRange(firstNum + 1, secNum);
+        }
+    }
+    else
+    {
+        Console.Write($"{firstNum} ");
+        if (firstNum != secNum) // условие для предотвращения бесконечной рекурсии
+        {
+            PrintNaturalNumbersRange(firstNum - 1, secNum);
+        }
+    }
+}
+
+Console.WriteLine("Input your first integer number: ");
+int firstInput = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input your second integer number: ");
+int secInput = Convert.ToInt32(Console.ReadLine());
+
+PrintNaturalNumbersRange(firstInput, secInput);
